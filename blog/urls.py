31e87f07205332c269da14bc42afa46dca95e8ce
raisa31 <raisaarief31@gmail.com>
2019-07-20 00:07:33 +0530

    Commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,20 @@
-#To map our file to url for each view function
 from django.urls import path
 from .views import (
-			PostListView, 
-			PostDetailView,
-			PostCreateView,
-			PostUpdateView,
-			PostDeleteView,
-			UserPostListView
-
-		)
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name = 'blog-home'),
-    path('user/<str:username>', UserPostListView.as_view(), name = 'user-posts'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post-detail'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = 'post-delete'),
-    path('post/new/', PostCreateView.as_view(), name = 'post-create'),
-    path('about/', views.about, name = 'blog-about'),
-
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('about/', views.about, name='blog-about'),
 ]
--- django_project/settings.py
+++ django_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig', 
+    'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
@@ -121,14 +121,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 LOGIN_REDIRECT_URL = 'blog-home'
-LOGIN_URL = 'login' 
-EMAIL_BACKED = 'django.core.mail.backends.smtp.EmailBackend'
+LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('USER_EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('USER_PASS')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('USERNAME')
-EMAIL_HOST_PASSWORD = os.environ.get('PASS')
